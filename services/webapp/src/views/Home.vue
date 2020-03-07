ed4f234bc33a1ef9e63ff185746d4cb734863cfd
Frederik Held <konten@frederikheld.de>
2020-03-07 14:04:21 +0100

    Refactors mqtt handling in Home.vue

--- services/webapp/src/views/Home.vue
+++ services/webapp/src/views/Home.vue
@@ -3,11 +3,17 @@
     <PlantCard
       v-for="(plant, plant_id) in plants"
       :key="plant_id"
+      class="plant_card"
       :plant="plant"
     />
   </div>
 </template>
 
+<style lang="scss" scoped>
+.plant_card {
+  margin: 4rem;
+}
+</style>
 <script>
 import PlantCard from '@/components/PlantCard.vue'
 
@@ -21,45 +27,48 @@ export default {
   components: { PlantCard },
   data () {
     return {
-      plants: {}
+      plants: {},
+      mqttClient: undefined
     }
   },
   mounted () {
     console.log('mounted')
-
-    const mqttClient = new Paho.Client(
-      '192.168.0.105',
-      Number(1884),
-      'PotPourriWebApp'
-    )
-    mqttClient.onConnectionLost = onConnectionLost
-    mqttClient.onMessageArrived = this.onMessageArrived
-
-    mqttClient.connect({ onSuccess: onConnect })
-
-    function onConnect () {
-      console.log('connected')
-      mqttClient.subscribe('devices/+/sensors/+', {
-        qos: 2
-      })
-    }
-
-    function onConnectionLost (response) {
-      console.log('connection lost')
-      console.log(response.errorCode + ' | ' + response.errorMessage)
-    }
+    this.mqttInit()
+    this.mqttConnect()
   },
   methods: {
+    mqttInit () {
+      this.mqttClient = new Paho.Client(
+        '192.168.0.105',
+        Number(1884),
+        'PotPourriWebApp'
+      )
+      this.mqttClient.onConnectionLost = this.onConnectionLost
+      this.mqttClient.onMessageArrived = this.onMessageArrived
+    },
+    mqttConnect () {
+      this.mqttClient.connect({ onSuccess: this.onConnect })
+    },
+    onConnect () {
+      // console.log('connected')
+      this.mqttClient.subscribe('devices/+/sensors/+', {
+        qos: 2
+      })
+    },
+    onConnectionLost (response) {
+      // console.log('connection lost')
+      console.log(response.errorCode + ' | ' + response.errorMessage)
+      this.mqttClient.connect({ onSuccess: this.onConnect })
+    },
     onMessageArrived (message) {
-      console.log('message arrived')
+      // console.log('message arrived')
       this.updatePlants(message)
     },
     updatePlants (message) {
-      console.log(message.payloadString)
-
       // extract device id:
       const deviceId = message.topic.match(/devices\/(.*)\/sensors\/.*/)[1]
-      console.log('device_id = ' + deviceId)
+
+      // console.log('[' + deviceId + '] ' + message.payloadString)
 
       const newPlants = this.plants
       newPlants[deviceId] = {
@@ -67,11 +76,7 @@ export default {
         humidity: message.payloadString
       }
 
-      console.log(newPlants)
-
       this.plants = Object.assign({}, newPlants)
-
-      console.log(this.plants)
     }
   }
 }
