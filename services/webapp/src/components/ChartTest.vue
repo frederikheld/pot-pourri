f1bb1adc177c28ecbfdb2c4d32b1001b8bb6b427
Frederik Held <konten@frederikheld.de>
2020-03-07 23:39:16 +0100

    Annotation is being updated if props change

--- services/webapp/src/components/ChartTest.vue
+++ services/webapp/src/components/ChartTest.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- <canvas ref="lineChart" /> -->
     <canvas ref="chart" />
     <div>{{ chartData }}</div>
   </div>
@@ -7,6 +8,7 @@
 
 <script>
 import Chart from 'chart.js'
+import * as chartjsPluginAnnotation from 'chartjs-plugin-annotation'
 
 export default {
   name: 'ChartTest',
@@ -21,28 +23,88 @@ export default {
   data () {
     return {
       chart: {},
-      formattedData: []
+      formattedData: [],
+      median: 0,
+      lineChart: {}
     }
   },
   watch: {
     chartData: function (newData, oldData) {
-      //   console.log('chartData updated!', newData)
+      // update chart data:
       this.formattedData = this.formatChartData(newData)
       this.chart.data.datasets = this.formattedData
+
+      // update annotation line:
+      this.chart.annotation.elements[
+        'current-value'
+      ].options.value = this.calculateMean(newData)
+      this.chart.annotation.elements['current-value'].options.label.content =
+        'mean = ' + Math.round(this.calculateMean(newData) * 100) / 100
+
+      // re-render chart:
       this.chart.update()
     }
   },
   mounted () {
     this.formattedData = this.formatChartData(this.chartData)
-    this.createChart(this.$refs.chart, this.formattedData)
-    // console.log(this.chart)
+    const cd = Object.assign({}, this.formattedData, [
+      {
+        type: 'line',
+        data: [0.5, 0.5, 0.5, 0.5],
+        borderWidth: 1,
+        borderColor: 'rgba(255,50,50,1.0)'
+      }
+    ])
+    this.createChart(this.$refs.chart, cd)
+
+    // this.lineChart = new Chart(this.$refs.lineChart, {
+    //   type: 'line',
+    //   data: {
+    //     labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    //     datasets: [
+    //       {
+    //         label: 'Prime and Fibonacci',
+    //         fillColor: 'rgba(220,220,220,0.2)',
+    //         strokeColor: 'rgba(220,220,220,1)',
+    //         pointColor: 'rgba(220,220,220,1)',
+    //         pointStrokeColor: '#fff',
+    //         pointHighlightFill: '#fff',
+    //         pointHighlightStroke: 'rgba(220,220,220,1)',
+    //         data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    //       },
+    //       {
+    //         label: 'My Second dataset',
+    //         fillColor: 'rgba(151,187,205,0.2)',
+    //         strokeColor: 'rgba(151,187,205,1)',
+    //         pointColor: 'rgba(151,187,205,1)',
+    //         pointStrokeColor: '#fff',
+    //         pointHighlightFill: '#fff',
+    //         pointHighlightStroke: 'rgba(151,187,205,1)',
+    //         data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    //       }
+    //     ]
+    //   },
+    //   options: {
+    //     // scales: {
+    //     //   yAxes: [
+    //     //     {
+    //     //       ticks: {
+    //     //         beginAtZero: true
+    //     //       }
+    //     //     }
+    //     //   ]
+    //     // }
+    //   }
+    // })
   },
   methods: {
+    calculateMean (data) {
+      const sum = data.reduce((a, b) => a + b, 0)
+      return sum / data.length
+    },
     formatChartData (data) {
-      //   console.log('data:', data)
       const formattedData = []
       data.forEach(function (value) {
-        // console.log('value:', value)
         formattedData.push({
           data: [value]
         })
@@ -50,9 +112,9 @@ export default {
       return formattedData
     },
     createChart (ctx, data) {
-      //   console.log('this.formattedData:', this.formattedData)
       this.chart = new Chart(ctx, {
         type: 'bar',
+        plugins: [chartjsPluginAnnotation],
         data: {
           datasets: this.formattedData
         },
@@ -66,9 +128,34 @@ export default {
           scales: {
             yAxes: [
               {
+                id: 'y-axis-0',
                 ticks: {
                   min: 0.0,
                   max: 1.0
+                }
+              }
+            ]
+          },
+          annotation: {
+            annotations: [
+              {
+                id: 'current-value',
+                type: 'line',
+                mode: 'horizontal',
+                scaleID: 'y-axis-0',
+                value: this.calculateMean(this.chartData),
+                borderWidth: 2,
+                borderColor: 'rgba(255,50,50,1.0)',
+                label: {
+                  content:
+                    'mean = ' +
+                    Math.round(this.calculateMean(this.chartData) * 100) / 100,
+                  position: 'center',
+                  fontSize: 16,
+                  backgroundColor: 'rgba(255,50,50,1.0)',
+                  yPadding: 10,
+                  xPadding: 10,
+                  enabled: true
                 }
               }
             ]
