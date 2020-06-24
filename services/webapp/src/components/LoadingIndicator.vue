94343d2f3c94e215e327bde7e91b5e1a670cb558
Frederik Held <git@frederikheld.de>
2020-06-25 01:34:02 +0200

    webapp: Adds loading indicator component

---
+++ services/webapp/src/components/LoadingIndicator.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-icon id="icon">
+      mdi-loading
+    </v-icon>
+  </div>
+</template>
+
+<style type="scss">
+#icon {
+  animation-name: spin;
+  animation-duration: 500ms;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+
+@keyframes spin {
+    from {
+        transform:rotate(0deg);
+    }
+    to {
+        transform:rotate(360deg);
+    }
+}
+</style>
+
+<script>
+export default {
+  name: 'LoadingIndicator'
+}
+</script>
