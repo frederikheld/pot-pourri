86337e44bbedfcbe1bb007bed327162e3f023fce
Frederik Held <git@frederikheld.de>
2020-06-28 14:57:02 +0200

    webapp: improves styling of settings form

--- services/webapp/src/views/Settings.vue
+++ services/webapp/src/views/Settings.vue
@@ -5,73 +5,87 @@
     />
     <v-container>
       <v-form>
+        <!-- section start -->
         <v-row>
           <v-col>
-            <span class="text-subtitle-1">
-              Network
-            </span>
+            <v-container
+              class="pa-0 form-section"
+            >
+              <v-row>
+                <v-col>
+                  <span class="text-h6">
+                    Network
+                  </span>
+                </v-col>
+              </v-row>
+              <!-- option start -->
+              <v-row
+                align="baseline"
+              >
+                <v-col
+                  cols="2"
+                >
+                  <span class="text-subtitle-2">MQTT:</span>
+                </v-col>
+                <v-col cols="3">
+                  <v-select
+                    v-model="form.network.mqtt.protocol"
+                    label="protocol"
+                    :items="['https', 'http']"
+                  />
+                </v-col>
+                <v-col
+                  cols="5"
+                >
+                  <v-text-field
+                    v-model="form.network.mqtt.address"
+                    label="hostname or ip"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-text-field
+                    v-model="form.network.mqtt.port"
+                    label="port"
+                  />
+                </v-col>
+              </v-row>
+              <!-- option end -->
+              <!-- option start -->
+              <v-row
+                align="baseline"
+              >
+                <v-col
+                  cols="2"
+                >
+                  <span class="text-subtitle-2">Metastore:</span>
+                </v-col>
+                <v-col cols="3">
+                  <v-select
+                    v-model="form.network.metastore.protocol"
+                    label="protocol"
+                    :items="['https', 'http']"
+                  />
+                </v-col>
+                <v-col
+                  cols="5"
+                >
+                  <v-text-field
+                    v-model="form.network.metastore.address"
+                    label="hostname or ip"
+                  />
+                </v-col>
+                <v-col cols="2">
+                  <v-text-field
+                    v-model="form.network.metastore.port"
+                    label="port"
+                  />
+                </v-col>
+              </v-row>
+              <!-- option end -->
+            </v-container>
           </v-col>
         </v-row>
-        <!-- option start -->
-        <v-row align="baseline">
-          <v-col
-            cols="2"
-          >
-            <span class="text-subtitle-2">MQTT:</span>
-          </v-col>
-          <v-col cols="3">
-            <v-select
-              v-model="form.network.mqtt.protocol"
-              label="protocol"
-              :items="['https', 'http']"
-            />
-          </v-col>
-          <v-col
-            cols="5"
-          >
-            <v-text-field
-              v-model="form.network.mqtt.address"
-              label="hostname or ip"
-            />
-          </v-col>
-          <v-col cols="2">
-            <v-text-field
-              v-model="form.network.mqtt.port"
-              label="port"
-            />
-          </v-col>
-        </v-row>
-        <!-- option end -->
-        <!-- option start -->
-        <v-row align="baseline">
-          <v-col
-            cols="2"
-          >
-            <span class="text-subtitle-2">Metastore:</span>
-          </v-col>
-          <v-col cols="3">
-            <v-select
-              v-model="form.network.metastore.protocol"
-              label="protocol"
-              :items="['https', 'http']"
-            />
-          </v-col>
-          <v-col
-            cols="5"
-          >
-            <v-text-field
-              v-model="form.network.metastore.address"
-              label="hostname or ip"
-            />
-          </v-col>
-          <v-col cols="2">
-            <v-text-field
-              v-model="form.network.metastore.port"
-              label="port"
-            />
-          </v-col>
-          <!-- option end -->
-        </v-row>
+        <!-- section end-->
         <v-row>
           <v-col class="text-right">
             <v-btn
@@ -89,6 +103,13 @@
     </v-container>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.form-section .col {
+    padding-top: 0;
+    padding-bottom: 0;
+}
+</style>
 
 <script>
 import AppBar from '@/components/AppBar.vue'
