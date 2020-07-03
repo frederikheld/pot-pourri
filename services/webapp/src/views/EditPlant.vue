20bf4f32f12d7ce62431f5cc2e4eebbe1bc8f1d2
Frederik Held <git@frederikheld.de>
2020-07-03 03:05:57 +0200

    webapp: can select and preview picture from local file system now
    Works seamlessly with the camera on mobile devices :-)

--- services/webapp/src/views/EditPlant.vue
+++ services/webapp/src/views/EditPlant.vue
@@ -12,6 +12,37 @@
         >
           <v-col>
             <v-row>
+              <v-img
+                :src="plantPicture"
+                aspect-ratio="2"
+                max-height="12rem"
+              >
+                <!-- <v-btn
+                  icon
+                  class="center-box"
+                >
+                  <v-icon
+                    color="white"
+                    x-large
+                  >
+                    mdi-camera
+                  </v-icon>
+                </v-btn> -->
+                <v-file-input
+                  v-model="form.picture"
+                  prepend-icon="mdi-camera"
+                  accept="image/png, image/jpeg, image/bmp"
+                  hide-input
+                  light
+                  class="center-box"
+                  style="color: #fff;"
+                  @change="updatePicture"
+                />
+              </v-img>
+            </v-row>
+          </v-col>
+          <v-col>
+            <v-row>
               <v-text-field
                 v-model="form.id"
                 label="Unique ID of the plant"
@@ -46,9 +77,26 @@
   </div>
 </template>
 
+<style lang="scss" scoped>
+.center-box {
+  display: block;
+  position: relative;
+  width: 24px;
+  height: 24px;
+  left: calc(50% - 12px);
+  top: calc(50% - 12px);
+  transition: transform .2s;
+}
+
+.center-box:hover {
+  transform: scale(1.1);
+}
+</style>
+
 <script>
 import AppBar from '@/components/AppBar.vue'
 import LoadingIndicator from '@/components/LoadingIndicator.vue'
+import * as loadImage from 'blueimp-load-image'
 
 import { mapGetters } from 'vuex'
 
@@ -60,7 +108,9 @@ export default {
       fetchingPlant: false,
       savingPlant: false,
       plant: {},
+      plantPicture: 'https://images.pexels.com/photos/4505146/pexels-photo-4505146.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
       form: {
+        picture: undefined,
         id: undefined,
         name: undefined
       }
@@ -98,6 +148,25 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    getPictureURL () {
+      if (this.form.picture) {
+        return URL.createObjectURL(this.form.picture)
+      } else {
+        return 'https://images.pexels.com/photos/4505146/pexels-photo-4505146.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      }
+    },
+    updatePicture (data) {
+      loadImage(
+        data,
+        (image) => {
+          this.plantPicture = image.toDataURL()
+        },
+        {
+          meta: true,
+          orientation: true,
+          canvas: true
+        })
     },
     onSubmit () {
       this.savingPlant = true
