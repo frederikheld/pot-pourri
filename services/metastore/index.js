6d5f27d6f0f71d041573a040861aeecdd6984bcd
Frederik Held <git@frederikheld.de>
2020-08-23 15:30:58 +0200

    Adds comments to explain context

--- services/metastore/index.js
+++ services/metastore/index.js
@@ -2,6 +2,9 @@
 
 /**
  * This script starts the server for production.
+ *
+ * It is designed to run inside the Docker container
+ * specified in `Dockerfile`.
  */
 
 const server = require('./server')
@@ -10,6 +13,7 @@ try {
   // Docker-internal hostname and port here!
   server.initDB('mongodb://mongodb:27017/pot-pourri')
 
+  // Docker-internal relative path here!
   server.initBlobStorage('./store/blob')
 } catch (error) {
   console.error('ERROR: ' + error)
