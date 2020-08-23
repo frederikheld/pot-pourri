77a7718506c231c73a3c9ef0880c39535cff2e52
Frederik Held <git@frederikheld.de>
2020-08-23 15:21:32 +0200

    adds missing initBlobStorage on server startup

--- services/metastore/index.js
+++ services/metastore/index.js
@@ -9,6 +9,8 @@ const server = require('./server')
 try {
   // Docker-internal hostname and port here!
   server.initDB('mongodb://mongodb:27017/pot-pourri')
+
+  server.initBlobStorage('./store/blob')
 } catch (error) {
   console.error('ERROR: ' + error)
 }
