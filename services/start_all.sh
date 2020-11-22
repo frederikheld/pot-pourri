c6e5a7558bb1a5ae2bb06db8618ee5f8c32e0476
Frederik Held <git@frederikheld.de>
2020-11-22 23:57:44 +0100

    Fixes chart and deployment

--- services/start_all.sh
+++ services/start_all.sh
@@ -2,25 +2,25 @@
 
 # start mqtt-broker
 cd mqtt-broker
-docker-compose up -d --force-recreate
+docker-compose up -d --build --force-recreate
 cd ..
 
 # start persistence
 cd persistence
-docker-compose up -d --force-recreate
+docker-compose up -d --build --force-recreate
 cd ..
 
 # start visualization
 cd visualization
-docker-compose up -d --force-recreate
+docker-compose up -d --build --force-recreate
 cd ..
 
 # start metastore
 cd metastore
-docker-compose up -d --force-recreate
+docker-compose up -d --build --force-recreate
 cd ..
 
 # start webapp
 cd webapp
-docker-compose up -d --force-recreate
+docker-compose up -d --build --force-recreate
 cd ..
